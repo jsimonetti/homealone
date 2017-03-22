c413c478855b6afcda9c90742e108a02fce0c4e9
Jeroen Simonetti <jeroen.simonetti@securelink.nl>
2017-03-22 12:52:36 +0100

    [device] add rotary component

---
+++ protocol/device/rotary.go
@@ -0,0 +1,20 @@
+package device
+
+import (
+	"encoding/gob"
+)
+
+// Used to make sure the interface is met
+var _ Component = &Rotary{}
+
+// Rotary is a rotaryencoder
+type Rotary struct {
+	Name string
+}
+
+// component is an empty method to comply to the interface Component
+func (Rotary) component() {}
+
+func init() {
+	gob.Register(Rotary{})
+}
