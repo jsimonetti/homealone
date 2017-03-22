3140f915f56d8d164cda0dfbd926ae7f11455424
Jeroen Simonetti <jeroen.simonetti@securelink.nl>
2017-03-22 14:57:19 +0100

    [doc] add documentation for drivers

---
+++ cmd/drivers/doc.go
@@ -0,0 +1,7 @@
+// Package drivers is a stub for Documentation of drivers
+//
+// Drivers are standalone applications that control devices.
+// A driver is an app which registers its' devices to the inventory
+// and responds to commands to devices under its' control
+//
+package drivers
