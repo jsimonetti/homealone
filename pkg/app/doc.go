84670ddd8df5447004f36fb0ab36d07ff39abe7d
Jeroen Simonetti <jeroen.simonetti@securelink.nl>
2017-03-22 15:20:44 +0100

    [doc] move driver doc to app

--- pkg/app/doc.go
+++ pkg/app/doc.go
@@ -1,2 +1,7 @@
 // Package app contains helper functions to create application and drivers
+//
+// Drivers are standalone applications that control devices.
+// A driver is an app which registers its' devices to the inventory
+// and responds to commands to devices under its' control
+//
 package app
