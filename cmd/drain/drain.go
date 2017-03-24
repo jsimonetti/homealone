d4c3ea77c466b6a0d5d862e956da1cd739546c40
Jeroen Simonetti <jeroen@simonetti.nl>
2017-03-24 20:18:44 +0100

    [drain] start with better printing

--- cmd/drain/drain.go
+++ cmd/drain/drain.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/jsimonetti/homealone/pkg/app"
@@ -35,7 +33,31 @@ func main() {
 
 // dumpHandler is the handler to deal with all messages
 func dumpHandler(m message.Message) error {
-	fmt.Printf("%s\t", time.Now())
-	spew.Dump(m)
+	switch m := m.(type) {
+	case *message.Discover:
+		spew.Dump(m)
+
+	case *message.Register:
+		spew.Dump(m)
+
+	case *message.Unregister:
+		spew.Dump(m)
+
+	case *message.Inventory:
+		spew.Dump(m)
+
+	case *message.InventoryReply:
+		spew.Dump(m)
+
+	case *message.Command:
+		spew.Dump(m)
+
+	case *message.CommandReply:
+		spew.Dump(m)
+
+	case *message.Event:
+		spew.Dump(m)
+	}
+
 	return nil
 }
