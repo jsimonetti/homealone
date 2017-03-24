428872cc2fd0907e38c398fdd74683347f88a8f8
Jeroen Simonetti <jeroen@simonetti.nl>
2017-03-24 02:47:16 +0100

    [doc] wrap comments

--- pkg/protocol/message/command.go
+++ pkg/protocol/message/command.go
@@ -45,21 +45,27 @@ func (CommandReply) message() {}
 
 //go:generate stringer -type=CommandResult
 
-// CommandResult is used to signal the sending partner about the result of this command
+// CommandResult is used to signal the sending partner about the result
+// of this command
 type CommandResult uint8
 
 const (
-	// CommandSyncAck is used to acknowledge receipt of the command and successful execution
+	// CommandSyncAck is used to acknowledge receipt of the command and
+	// successful execution
 	CommandSyncAck CommandResult = iota
-	// CommandSyncFail is used to acknowledge receipt of the command and unsuccessful execution
+	// CommandSyncFail is used to acknowledge receipt of the command and
+	// unsuccessful execution
 	CommandSyncFail
-	// CommandAsyncAck is used to acknowledge receipt of the command but immediate result is not available
-	// The caller should monitor the command queue to wait for the async result
+	// CommandAsyncAck is used to acknowledge receipt of the command but
+	// immediate result is not available. The caller should monitor the
+	// command queue to wait for the async result
 	CommandAsyncAck
-	// CommandAsyncFail is used to signal failure on an async command. Message should be consulted to see the reason.
+	// CommandAsyncFail is used to signal failure on an async command.
+	// Message should be consulted to see the reason.
 	CommandAsyncFail
 	// CommandAsyncSuccess is used to signal success on an async command
 	CommandAsyncSuccess
-	// CommandError is used to signal a failure with executing the command. Message should be consulted to see the reason.
+	// CommandError is used to signal a failure with executing the command.
+	// Message should be consulted to see the reason.
 	CommandError
 )
